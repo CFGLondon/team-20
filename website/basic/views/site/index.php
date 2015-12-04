ac88341988333a7087947cb2fda564c747cdf821
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-04 23:32:42 +0000

    Testing CI again

--- website/basic/views/site/index.php
+++ website/basic/views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>Congratulations!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application. TEST !!!!</p>
+        <p class="lead">You have successfully created your Yii-powered application. TEST !!!! 222</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>
