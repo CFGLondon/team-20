231d87ff275932c9179b73bc44f2f025f80d90a5
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 02:39:38 +0000

    SMS should now allow auto checking

--- twiliotest/sms/sms-hello-monkey.php
+++ twiliotest/sms/sms-hello-monkey.php
@@ -22,7 +22,7 @@
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
 	// Check connection
-	$sql = "INSERT INTO Report(``disability_prose`,`location_prose`,`problem_prose`,`lat`,`long`,`location_is_precise`,`time_sent`,`requires_editing`,`is_solved`,`time_updated`,`sms_id`,`name`) 
+	$sql = "INSERT INTO Report(`disability_prose`,`location_prose`,`problem_prose`,`lat`,`long`,`location_is_precise`,`time_sent`,`requires_editing`,`is_solved`,`time_updated`,`sms_id`,`name`) 
 	VALUES('$parsed[1]','$parsed[0]','$parsed[2]',1.0,1.0,0,NOW(),$checkflag,0,NOW(),1,'$nameout');";
 	$conn->query($sql);
 	$conn->close();
