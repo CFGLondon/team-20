33c15c7004cb858b756f942a6fb107759b10a9fe
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-05 03:34:58 +0000

    Changed the application name in the config

--- website/basic/config/web.php
+++ website/basic/config/web.php
@@ -4,6 +4,7 @@ $params = require(__DIR__ . '/params.php');
 
 $config = [
     'id' => 'basic',
+    'name'=> 'Code4Good by Dirty Bits',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
