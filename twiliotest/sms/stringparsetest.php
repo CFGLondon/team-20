278d83fc4ea1f669cd310e03b270f6be0a5b1790
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 00:31:03 +0000

    Added testing script for string parsing

--- twiliotest/sms/stringparsetest.php
+++ twiliotest/sms/stringparsetest.php
@@ -3,6 +3,7 @@
 	$smsstring = "Singapore / deaf / no help at store / nicholas";
 	$checkflag = checkparse($smsstring);
 	$result = parsetolist($smsstring);
+	echo "hello";
 	echo $result(0);
 	echo $checkflag;
 	}
