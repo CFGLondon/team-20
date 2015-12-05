406f58e6f62ba599ca183dc3e330504e2c496be3
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-05 07:54:37 +0000

    got about example working again for reference

--- website/basic/views/site/about.php
+++ website/basic/views/site/about.php
@@ -12,21 +12,47 @@ use dosamigos\google\maps\services\DirectionsRequest;
 use dosamigos\google\maps\overlays\Polygon;
 use dosamigos\google\maps\layers\BicyclingLayer;
  
-$coord = new LatLng(['lat' => 52.1999722, 'lng' => 0.1247423]);
+$coord = new LatLng(['lat' => 39.720089311812094, 'lng' => 2.91165944519042]);
 $map = new Map([
     'center' => $coord,
-    'zoom' => 4,
+    'zoom' => 14,
 ]);
  
-
+// lets use the directions renderer
+$home = new LatLng(['lat' => 39.720991014764536, 'lng' => 2.911801719665541]);
+$school = new LatLng(['lat' => 39.719456079114956, 'lng' => 2.8979293346405166]);
+$santo_domingo = new LatLng(['lat' => 39.72118906848983, 'lng' => 2.907628202438368]);
+ 
+// setup just one waypoint (Google allows a max of 8)
+$waypoints = [
+    new DirectionsWayPoint(['location' => $santo_domingo])
+];
+ 
+$directionsRequest = new DirectionsRequest([
+    'origin' => $home,
+    'destination' => $school,
+    'waypoints' => $waypoints,
+    'travelMode' => TravelMode::DRIVING
+]);
+ 
+// Lets configure the polyline that renders the direction
+$polylineOptions = new PolylineOptions([
+    'strokeColor' => '#FFAA00',
+    'draggable' => true
+]);
+ 
 // Now the renderer
 $directionsRenderer = new DirectionsRenderer([
     'map' => $map->getName(),
     'polylineOptions' => $polylineOptions
 ]);
  
-
-
+// Finally the directions service
+$directionsService = new DirectionsService([
+    'directionsRenderer' => $directionsRenderer,
+    'directionsRequest' => $directionsRequest
+]);
+ 
 // Thats it, append the resulting script to the map
 $map->appendScript($directionsService->getJs());
  
@@ -75,4 +101,5 @@ $map->appendScript($bikeLayer->getJs());
  
 // Display the map -finally :)
 echo $map->display();
+
 
