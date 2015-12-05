1604d0ec5555a3b2d8f1bffbfe18a695335111f8
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 05:33:05 +0000

    Refactored hello-monkey to reduce SQL connections

--- twiliotest/sms/stringparse.php
+++ twiliotest/sms/stringparse.php
@@ -1,6 +1,6 @@
 <?php
 	function checkparse($inputstring1){
-		$outputarray1 = explode("/",$inputstring1);
+		$outputarray1 = explode('/',$inputstring1);
 		$result = count($outputarray1);
 		if ($result > 4 || $result < 3){
 			return 1;
