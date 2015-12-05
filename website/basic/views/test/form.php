8eaf253bc3b5d40c0888c21e835d9959d197c2bf
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-05 09:53:51 +0000

    Testing adding searchbox to google maps

--- website/basic/views/test/form.php
+++ website/basic/views/test/form.php
@@ -42,6 +42,18 @@ function initialize() {
     center: latLng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+
   var marker = new google.maps.Marker({
     position: latLng,
     title: 'Point A',
