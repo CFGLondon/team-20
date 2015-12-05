bd319a4e827252f9b2ff0de837fe60a8218bcca6
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 04:00:42 +0000

    Testing pure string to location function

--- twiliotest/sms/stringparsetest.php
+++ twiliotest/sms/stringparsetest.php
@@ -2,7 +2,7 @@
     include 'mapsclass.php';
     $address=urlencode("1600 Amphitheatre Parkway, Mountain View, CA");
     $loc = geocoder::getLocation($address);
-    print_r($loc[lat]);
+    print_r($loc);
 
 
 ?>
