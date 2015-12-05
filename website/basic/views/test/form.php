173c443eeb215b8b38d43e8f6915b46678e3cabf
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-05 11:03:59 +0000

    Restoring form to FORMer glory

--- website/basic/views/test/form.php
+++ website/basic/views/test/form.php
@@ -1,16 +1,13 @@
 <?php
-
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
 use kartik\select2\Select2;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\Report */
 /* @var $form ActiveForm */
 $this->title = "Form";
 ?>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?libraries=places&sensor=false"></script>
 <script type="text/javascript">
 var geocoder = new google.maps.Geocoder();
 </script>
@@ -26,54 +23,87 @@ function geocodePosition(pos) {
     }
   });
 }
-
 function updateMarkerPosition(latLng) {
   document.getElementById('report-long').value =  latLng.lng();
   document.getElementById('report-lat').value =  latLng.lat();
 }
-
 function updateMarkerAddress(str) {
   document.getElementById('report-location_prose').value = str;
 }
-
 function initialize() {
   var latLng = new google.maps.LatLng(10.0, 25.0);
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('mapCanvas'), {
+    zoom: 4,
     center: latLng,
-    zoom: 3,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-  })
-
-  // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
-  var searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
   });
-
-
   var marker = new google.maps.Marker({
     position: latLng,
     title: 'Point A',
     map: map,
     draggable: true
   });
-
   // Update current position info.
   updateMarkerPosition(latLng);
   geocodePosition(latLng);
-
   // Add dragging event listeners.
   google.maps.event.addListener(marker, 'drag', function() {
     updateMarkerPosition(marker.getPosition());
   });
-
   google.maps.event.addListener(marker, 'dragend', function() {
     geocodePosition(marker.getPosition());
   });
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+  var markers = [];
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+    marker.setPosition(bounds.getCenter());
+    updateMarkerPosition(marker.getPosition());
+  });
+  // [END region_getplaces]
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 </script>
@@ -88,7 +118,7 @@ google.maps.event.addDomListener(window, 'load', initialize);
     <?php $form = ActiveForm::begin(); ?>
 
         <div id="form_get_lat_long">
-            <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+            <input id="pac-input" class="controls" type="text" placeholder="Search Box" />
             <div id="mapCanvas"></div>
             <?= $form->field($model, 'lat') ?>
             <?= $form->field($model, 'long') ?>
