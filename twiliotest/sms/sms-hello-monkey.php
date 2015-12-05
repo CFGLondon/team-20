f91e4ba2dceb76842a3bdaea790b2853edb7533b
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 00:04:38 +0000

    Added functionality to parse sms input

--- twiliotest/sms/sms-hello-monkey.php
+++ twiliotest/sms/sms-hello-monkey.php
@@ -2,8 +2,12 @@
     include 'stringparse.php';
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-	$smsstring=$_REQUEST['Body']
+	$smsstring=$_REQUEST['Body'];
+	$checkflag = checkparse($smsstring);
+	$parsed = parsetolist($smsstring);
+	
+	
 ?>
 <Response>
-    <Message>What you said is:<?php echo $smsstring?></Message>
+    <Message><?php echo $parsed ?>Thank you for contacting us!</Message>
 </Response>
--- twiliotest/sms/stringparse.php
+++ twiliotest/sms/stringparse.php
@@ -1,10 +1,17 @@
 <?php
-	function parsetolist($inputstring){
-		
-		return $outputarray
+	function checkparse($inputstring1){
+		$outputarray1 = explode("/",$inputstring2);
+		$result = count($outputarray1);
+		if ($outputarray1 > 3 || $outputarray1 < 2){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
-	function checkparse($inputstring){
-		
+	function parsetolist($inputstring2){
+		 $outputarray2 = explode("/",$inputstring2);
+		return $outputarray2;
 	}
 
 
