953915d9b5af3a05bed7e3eb79d317036987b0c3
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 00:45:18 +0000

    testing parsing

--- twiliotest/sms/stringparsetest.php
+++ twiliotest/sms/stringparsetest.php
@@ -5,7 +5,8 @@
 	$smsstring = "Singapore / deaf / no help at store / nicholas ";
 	$checkflag = checkparse($smsstring);
 	$result = parsetolist($smsstring);
-	echo $result(0);
+	echo gettype($result);
+	echo $result;
 
 
 ?>
