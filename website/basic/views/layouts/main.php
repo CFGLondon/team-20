4a05bafa7c79a649c5b79f80e8e052bb2a2178d7
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-05 02:50:02 +0000

    Added links to nav bar

--- website/basic/views/layouts/main.php
+++ website/basic/views/layouts/main.php
@@ -37,8 +37,8 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Map', 'url' => ['/test/map']],
+            ['label' => 'Contact', 'url' => ['/test/form']],
             Yii::$app->user->isGuest ?
                 ['label' => 'Login', 'url' => ['/site/login']] :
                 [
