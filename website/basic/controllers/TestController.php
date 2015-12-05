899ba0da6df1073581a0238cdc2deaacca32f12d
Tom Read-Cutting <tr395@cam.ac.uk>
2015-12-05 05:23:37 +0000

    Added datetime to form namespace and added more select2

--- website/basic/controllers/TestController.php
+++ website/basic/controllers/TestController.php
@@ -11,6 +11,7 @@ use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use DateTime;
 
 /**
  * TestController implements the CRUD actions for Report model.
