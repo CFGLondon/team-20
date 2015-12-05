d023eaac1fbd20cadc614cec90293306f11290f5
Nicholas Capel <nrjc2@cam.ac.uk>
2015-12-05 00:17:32 +0000

    Tried to integrate parsing code with database code for SMS

--- twiliotest/sms/sms-hello-monkey.php
+++ twiliotest/sms/sms-hello-monkey.php
@@ -1,13 +1,25 @@
 <?php
+	$servername = "ec2-54-170-43-20.eu-west-1.compute.amazonaws.com";
+	$username = "root";
+	$password = "code4good";
+	$dbname = "add_db";
+
     include 'stringparse.php';
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	$smsstring=$_REQUEST['Body'];
 	$checkflag = checkparse($smsstring);
 	$parsed = parsetolist($smsstring);
-	
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	$sql = "INSERT INTO DisabilityCategory (category)
+	VALUES ('Test')";
+	$conn->query($sql);
+
+	$conn->close();
 	
 ?>
 <Response>
-    <Message><?php echo $parsed(0) ?>Thank you for contacting us!</Message>
+    <Message>Thank you for contacting us!</Message>
 </Response>
